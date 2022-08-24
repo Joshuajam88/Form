e789bb949b96a163eefe53726e72cc14809dfee4
Joshuajam88 <jj6787409@gmail.com>
2022-08-24 19:38:08 +0200

    New changes

--- Form.js
+++ Form.js
@@ -9,6 +9,7 @@
 
 
 
+// validation for name, email and password
 document.getElementById('Submit').addEventListener('click', function submitForm(){
     var name = document.getElementById('Name').value;
     var email = document.getElementById('Email').value
@@ -16,7 +17,6 @@ document.getElementById('Submit').addEventListener('click', function submitForm(
     var length = passWord.length
     var ValidateEmail = ValidateEmail.value
 
-// validation for name, email and password
     if(
         name == '',
         email == '', 
@@ -35,6 +35,7 @@ document.getElementById('Submit').addEventListener('click', function submitForm(
         }
         return true
     }
+
     // validation for @ in email
         function ValidateEmail(email) {
             if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
@@ -45,19 +46,40 @@ document.getElementById('Submit').addEventListener('click', function submitForm(
         }
         }
     });
-    
-    // let element = document.createElement('h1');
-    // let textNode = document.createTextNode('I have more than a song today, Great is they faithfulness');
-    // element.appendChild(textNode);
-    // document.getElementById('demo').appendChild(element);
+
+
+    // Consumption of Bible API to display of the website per seconds interval
+var data = JSON.stringify(false);
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+xhr.addEventListener("readystatechange", function(){
+    if(this.readyState === this.DONE){
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("Get", "https://api.scripture.api.bible/v1/bibles/biblesId/books/bookId/chapters/chaptersId/verses");
+xhr.setRequestHeader("api-key", "api-key");
+xhr.send(data);
+
+
+    // Creating the button element
+    var button = document.createElement('button');
+    button.innerHTML = "Google &#x92;"
+
+    // Append somewhere
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(button)
+
+    // Adding event handler 
+    button.addEventListener("click", function(){
+        alert("did something")
+    })
+
+
 
     // let ele = document.createElement('p');
     // let text = document.createTextNode('I give you all the Glory');
     // element.appendChild(text);
     // document.getElementById('demo').appendChild(ele);
-        
-    
-
-   
-
-     
